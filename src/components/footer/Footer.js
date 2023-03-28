118aed70aefc24a93938d774fafab03b46c32c03
Katrina <kalynndier@gmail.com>
2023-03-28 19:34:43 -0400

    pages and components started

---
+++ src/components/footer/Footer.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <footer>
+      <ul>
+        <li>
+          <a href="#">Facebook</a>
+        </li>
+        <li>
+          <a href="#">YouTube</a>
+        </li>
+        <li>
+          <a href="#">Pinterest</a>
+        </li>
+        <li>
+          <a href="#">Blog</a>
+        </li>
+      </ul>
+      <p>Copyright © 2023 Renaissance Wife | All Rights Reserved </p>
+    </footer>
+  );
+}
+
+export default Footer
---
+++ src/components/header/Header.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <header>
+      <nav>
+        <li>
+          <a href="#">Blog</a>
+        </li>
+        <li>
+          <a href="#">Events</a>
+        </li>
+        <li>
+          <a href="#">Gallery</a>
+        </li>
+        <li>
+          <a href="#">Contact</a>
+        </li>
+      </nav>
+    </header>
+  );
+}
+
+export default Header
