c666da8875a9aac1bc7f41b74ff254944d5ab7fd
Katrina <kalynndier@gmail.com>
2023-03-31 12:40:50 -0400

    contact page

--- src/views/contact/Contact.js
+++ src/views/contact/Contact.js
@@ -1,16 +1,48 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Layout from '../../components/layout/Layout'
 import './contact.scss'
 
 function Contact() {
+
+  const [inputValue, setInputValue] = useState('') 
+
+  const handleChange = (event) => {
+    setInputValue(event.target.value)
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+  }
+  
+
   return (
     <Layout>
       <section className="contact">
         <h1>CONTACT</h1>
+        <form onSubmit={handleSubmit} className="form">
+          <label>
+            <input
+              type="text"
+              value={inputValue}
+              placeholder="First name"
+              onChange={handleChange}
+            />
+          </label>
+
+          <label>
+            <input
+              type="text"
+              value={inputValue}
+              placeholder="Email address"
+              onChange={handleChange}
+            />
+          </label>
+
+          <button type="submit" className='button'>Submit</button>
+        </form>
       </section>
-    
     </Layout>
-  )
+  );
 }
 
 export default Contact
