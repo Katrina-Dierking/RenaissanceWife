54350309204db6e68f06763effa4e1241e54af93
Katrina <kalynndier@gmail.com>
2023-03-31 10:32:06 -0400

    routes added. 404 added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 
 import './App.scss';
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+
 import Home from './views/home/Home';
+import Events from './views/events/Events';
+import Contact from './views/contact/Contact';
 
 function App() {
   return (
-   <section className='app'>
-    <Home />
-   </section>
+    <>
+      <section className='app'>
+      <Home />
+      </section>
+
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Home/>} />
+          <Route path = "contact" element={<Contact/>} />
+          <Route path = "events" element ={<Events />} />
+        </Routes>
+      </BrowserRouter>
+    
+    </>
   );
 }
 
