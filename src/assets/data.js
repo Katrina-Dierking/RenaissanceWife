0772925d87e39dfa8e40494483695b15664af3a0
Katrina <kalynndier@gmail.com>
2023-04-08 19:54:58 -0400

    gallery added

---
+++ src/assets/data.js
@@ -0,0 +1,140 @@
+export const photos = [
+         {
+           id: 1,
+           title: "Lana Lane",
+           url:
+             "https://renaissancewife.com/wp-content/uploads/2023/04/gal1.jpg",
+         },
+         {
+           id: 2,
+           title: "The Dr is in",
+           url:
+             "https://renaissancewife.com/wp-content/uploads/2023/04/gal2.jpg",
+         },
+         {
+           id: 3,
+           title: "Bird Lights",
+           url:
+             "https://renaissancewife.com/wp-content/uploads/2023/04/gal3-scaled.jpg",
+         },
+         {
+           id: 4,
+           title: "Fall",
+           url:
+             "https://renaissancewife.com/wp-content/uploads/2023/04/gal4-scaled.jpg",
+         },
+         {
+           id: 5,
+           title: "Purple",
+           url:
+             "https://renaissancewife.com/wp-content/uploads/2023/04/gal5.jpg",
+         },
+         {
+           id: 6,
+           title: "Red tulips",
+           url:
+             "https://renaissancewife.com/wp-content/uploads/2023/04/gal6.jpg",
+         },
+         {
+           id: 7,
+           title: "Iris Lillies",
+           url:
+             "https://renaissancewife.com/wp-content/uploads/2023/04/gal7.jpg",
+         },
+         {
+           id: 8,
+           title: "white",
+           url:
+             "https://renaissancewife.com/wp-content/uploads/2023/04/gal8.jpg",
+         },
+         {
+           id: 9,
+           title: "tomatoes",
+           url:
+             "https://renaissancewife.com/wp-content/uploads/2023/04/gal9.jpg",
+         },
+         {
+           id: 10,
+           title: "Back porch",
+           url:
+             "https://renaissancewife.com/wp-content/uploads/2023/04/gal10-scaled.jpg",
+         },
+         {
+           id: 11,
+           title: "The studio",
+           url:
+             "https://renaissancewife.com/wp-content/uploads/2023/04/gal11-scaled.jpg",
+         },
+         {
+           id: 12,
+           title: "Bunny meditating",
+           url:
+             "https://renaissancewife.com/wp-content/uploads/2023/04/gal12-scaled.jpg",
+         },
+         {
+           id: 13,
+           title: "Mother and Daughter at the ball",
+           url:
+             "https://renaissancewife.com/wp-content/uploads/2023/04/gal13.jpg",
+         },
+         {
+           id: 14,
+           title: "Right out of Narnia",
+           url:
+             "https://renaissancewife.com/wp-content/uploads/2023/04/gal14.jpg",
+         },
+         {
+           id: 15,
+           title: "Freedom",
+           url:
+             "https://renaissancewife.com/wp-content/uploads/2023/04/gal15.jpg",
+         },
+         {
+           id: 16,
+           title: "Laberinth",
+           url:
+             "https://renaissancewife.com/wp-content/uploads/2023/04/gal16.jpg",
+         },
+         {
+           id: 17,
+           title: "4-wheeler fun",
+           url:
+             "https://renaissancewife.com/wp-content/uploads/2023/04/gal17.jpg",
+         },
+         {
+           id: 18,
+           title: "Bunt cake",
+           url:
+             "https://renaissancewife.com/wp-content/uploads/2023/04/gal18.jpg",
+         },
+         {
+           id: 19,
+           title: "Side porch",
+           url:
+             "https://renaissancewife.com/wp-content/uploads/2023/04/gal19.jpg",
+         },
+         {
+           id: 20,
+           title: "Lana and husband",
+           url:
+             "https://renaissancewife.com/wp-content/uploads/2023/04/gal20.jpg",
+         },
+         {
+           id: 21,
+           title: "Sunflowers",
+           url:
+             "https://renaissancewife.com/wp-content/uploads/2023/04/gal21.jpg",
+         },
+         {
+           id: 22,
+           title: "Lana thinking",
+           url:
+             "https://renaissancewife.com/wp-content/uploads/2023/04/gal22.jpg",
+         },
+         {
+           id: 23,
+           title: "Lana",
+           url:
+             "https://renaissancewife.com/wp-content/uploads/2023/04/gal23.jpg",
+         },
+       ];
--- src/views/gallery/Gallery.js
+++ src/views/gallery/Gallery.js
@@ -1,15 +1,64 @@
-import React from 'react'
+import React, {useState} from 'react'
+import {photos} from '../../assets/data'
+import './gallery.scss'
 import Layout from '../../components/layout/Layout'
 
 function Gallery() {
-  return (
-    <Layout>
-      <section className="gallery">
-        <h1>GALLERY</h1>
-      </section>
-    
-    </Layout>
-  )
+    const [currentIndex, setCurrentIndex] = useState(0)
+
+    const next = () => {
+        setCurrentIndex((currentIndex + 1) % photos.length)
+    }
+
+    const prev = () => {
+        setCurrentIndex((currentIndex - 1 + photos.length) % photos.length )
+    }
+
+    return (
+      <>
+      <Layout>
+      
+        <section className="slider-container">
+          {photos.map((photo) => (
+            <div
+              key={photo.id}
+              // if the photo is the current photo, show it
+              className={
+                photos[currentIndex].id === photo.id ? "fade" : "slide fade"
+              }
+            >
+              <img src={photo.url} alt={photo.title} className="photo" />
+              <div className="caption">{photo.title}</div>
+            </div>
+          ))}
+
+          {/* Previous button */}
+          <button onClick={prev} className="prev">
+            &lt;
+          </button>
+
+          {/* Next button */}
+          <button onClick={next} className="next">
+            &gt;
+          </button>
+        </section>
+
+        <section className="dots">
+          {photos.map((photo) => (
+            <span
+              key={photo.id}
+              // highlight the dot that corresponds to the current photo
+              className={
+                photos[currentIndex].id === photo.id ? "dot active" : "dot"
+              }
+              // when the user clicks on a dot, go to the corresponding photo
+              onClick={() => setCurrentIndex(photos.indexOf(photo))}
+            ></span>
+          ))}
+        </section>
+      </Layout>
+      </>
+    );
 }
 
 export default Gallery
